37dbce1b4ab867edbecd07cae834c58f3211cd94
Petru Giurgiu <petru@paymoapp.com>
2021-10-04 12:16:02 +0300

    add-loading-css-componnent

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "DevProvider", {
+  enumerable: true,
+  get: function get() {
+    return _DevProvider.default;
+  }
+});
+
+var _DevProvider = _interopRequireDefault(require("./components/DevProvider"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+import DevProvider from "./components/DevProvider"
+import Loading from "./components/Loding/Loading"
+
+export { DevProvider, Loading }
